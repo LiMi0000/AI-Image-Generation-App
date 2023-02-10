2041e8f3566f1f6df65da327a476662cb37b3c5e
LiMi <trestarssales@gmail.com>
2023-02-10 20:13:06 +0100

    working on connecting fe with be, generating images works now, fixing some bugs

--- client/src/page/CreatePost.jsx
+++ client/src/page/CreatePost.jsx
@@ -15,11 +15,35 @@ const CreatePost = () => {
   const [generatingImg, setGeneratingImg] = useState(false);
   const [loading, setLoading] = useState(false);
 
-  const generateImage = () => {};
-  const handleSubmit = () => {};
-  const handleChange = (e) => {
-    setForm({ ...form, [e.target.name]: e.target.value });
+  const generateImage = async () => {
+    if (form.prompt) {
+      try {
+        setGeneratingImg(true);
+        const response = await fetch('http://localhost:8080/api/v1/dalle', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ prompt: form.prompt }),
+        });
+
+        const data = await response.json();
+
+        setForm({ ...form, photo: `data:image/jpeg;base64,${data.photo}` });
+      } catch (error) {
+        alert(error);
+      } finally {
+        setGeneratingImg(false);
+      }
+    } else {
+      alert('Please enter a prompt');
+    }
   };
+  const handleSubmit = () => {};
+
+  const handleChange = (e) =>
+    setForm({ ...form, [e.target.name]: e.target.value });
+
   const handleSurpriseMe = () => {
     const randomPrompt = getRandomPrompt(form.prompt);
     setForm({ ...form, prompt: randomPrompt });
@@ -41,7 +65,7 @@ const CreatePost = () => {
             labelName="Your name"
             type="text"
             name="name"
-            placeholder="John Doe"
+            placeholder="e.g John Doe"
             value={form.name}
             handleChange={handleChange}
           />
